8de049782426e72a4aac7501faa28a04d96a4af7
RMYanenko <furnitex.roman@gmail.com>
2019-11-22 15:51:11 +0200

    Section 3.16

--- src/App.js
+++ src/App.js
@@ -13,7 +13,19 @@ class App extends Component {
     pageTitle: 'React components'
   }
 
+  changeTitleHandler = () => {
+
+    const oldTitle = this.state.pageTitle
+
+    const newTitle = oldTitle + ' (change)'
+
+    this.setState({
+      pageTitle: newTitle
+    })
+  }
+
   render() {
+    console.log('render')
     const divStyle = {
       textAlign: 'center'
     }
@@ -23,6 +35,7 @@ class App extends Component {
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
+        <button onClick={this.changeTitleHandler}>Change title</button>
 
         <Car name={cars[0].name} year={cars[0].year} />
         <Car name={cars[1].name} year={cars[1].year} />
