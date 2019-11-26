7ca46aad57f5557e822ff53df1f5773f3024dd77
RMYanenko <furnitex.roman@gmail.com>
2019-11-26 17:55:20 +0200

    Section 3.17

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default props => (
     <div>
         <h3>Car name: {props.name}</h3>
-        <p>Yaer: {props.year}</p>        
+        <p>Yaer: {props.year}</p>
+        <button onClick={props.onChangeTitle}>Click</button>   
     </div>
 )
