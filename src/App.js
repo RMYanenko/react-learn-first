caad59cf7e5c088058947f322757333afd44a5f0
RMYanenko <furnitex.roman@gmail.com>
2019-11-27 14:08:20 +0200

    Section 3.18

--- src/App.js
+++ src/App.js
@@ -1,52 +1,70 @@
-import React, { Component } from 'react';
-import './App.css';
-import Car from './Car/Car'
+import React, { Component } from "react";
+import "./App.css";
+import Car from "./Car/Car";
 
 class App extends Component {
-
   state = {
     cars: [
-      {name: 'Ford', year: 2018},
-      {name: 'Audi', year: 2016},
-      {name: 'Mazda', year: 2010}
+      {
+        name: "Ford",
+        year: 2018
+      },
+      {
+        name: "Audi",
+        year: 2016
+      },
+      {
+        name: "Mazda",
+        year: 2010
+      }
     ],
-    pageTitle: 'React components'
-  }
+    pageTitle: "React components"
+  };
 
-  changeTitleHandler = (newTitle) => {
+  changeTitleHandler = newTitle => {
     this.setState({
       pageTitle: newTitle
-    })
-  }
+    });
+  };
+
+  handelInput = event => {
+    this.setState({
+      pageTitle: event.target.value
+    });
+  };
 
   render() {
-    console.log('render')
+    console.log("render");
     const divStyle = {
-      textAlign: 'center'
-    }
+      textAlign: "center"
+    };
 
-    const cars = this.state.cars
+    const cars = this.state.cars;
 
     return (
       <div style={divStyle}>
-        <h1>{this.state.pageTitle}</h1>
-        <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}
-        >Change title</button>
+        <h1> {this.state.pageTitle} </h1>
 
-        <Car 
+        <input type="text" onChange={this.handelInput} />
+
+        <button onClick={this.changeTitleHandler.bind(this, "Changed!")}>
+          Change title{" "}
+        </button>
+
+        <Car
           name={cars[0].name}
-          year={cars[0].year} 
+          year={cars[0].year}
           onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
-          />
-        <Car 
+        />
+        <Car
           name={cars[1].name}
           year={cars[1].year}
-          onChangeTitle={()=> this.changeTitleHandler(cars[1].name)}
+          onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
         />
-        <Car 
+        <Car
           name={cars[2].name}
           year={cars[2].year}
-          onChangeTitle={()=> this.changeTitleHandler(cars[2].name)}
+          onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
         />
       </div>
     );
