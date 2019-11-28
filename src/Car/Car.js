59002dace347fde9df55ffeb7963549a2491df85
RMYanenko <furnitex.roman@gmail.com>
2019-11-28 15:52:48 +0200

    Section 4(2)

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./Car.css";
 
-export default props => (
-  <div className="Car">
-    <h3>Car name: {props.name}</h3>
-    <p>Yaer: {props.year}</p>
-    <input type="text" onChange={props.onChangeName} value={props.name} />
-    <button onClick={props.onDelete}>Delete</button>
-  </div>
-);
+export default props => {
+  const inputClasses = ["input"];
+
+  if (props.name !== "") {
+    inputClasses.push("green");
+  } else {
+    inputClasses.push("red");
+  }
+
+  if (props.name.length > 4) {
+    inputClasses.push("bold");
+  }
+
+  return (
+    <div className="Car">
+      <h3>Car name: {props.name}</h3>
+      <p>Yaer: {props.year}</p>
+      <input
+        type="text"
+        onChange={props.onChangeName}
+        value={props.name}
+        className={inputClasses.join(" ")}
+      />
+      <button onClick={props.onDelete}>Delete</button>
+    </div>
+  );
+};
